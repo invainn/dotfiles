cafd1be1b9cc31b47ff7cb52919edd3df7657b78
Anthony Bennett <goodoneanthony@gmail.com>
2022-12-17 08:00:57 -0800

    feat: added prime's setup for fugitive

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,4 +1,25 @@
 vim.keymap.set('n', '<Leader>gs', vim.cmd.Git)
-vim.keymap.set('n', '<Leader>gP', function()
-  vim.cmd.Git('push')
-end)
+
+local fugitive = vim.api.nvim_create_augroup('fg-group', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('BufWinEnter', {
+  group = fugitive,
+  pattern = "*",
+  callback = function()
+    if vim.bo.ft ~= 'fugitive' then
+      return
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set('n', '<Leader>P', function()
+      vim.cmd.Git('push')
+    end, opts)
+
+    vim.keymap.set('n', '<Leader>p', function()
+      vim.cmd.Git('pull')
+    end, opts)
+  end
+})
