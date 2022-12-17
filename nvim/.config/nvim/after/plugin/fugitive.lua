e75c6d1a4b10c1ed043fd036bbc942ba6a339421
Anthony Bennett <goodoneanthony@gmail.com>
2022-12-17 07:43:34 -0800

    feat: add git push

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,5 +1,4 @@
--- local status, fugitive = pcall(require, 'fugitive')
--- if (not status) then return end
---
--- print('hello')
-vim.keymap.set('n', '<Leader>gf', vim.cmd.Git)
+vim.keymap.set('n', '<Leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<Leader>gP', function()
+  vim.cmd.Git('push')
+end)
