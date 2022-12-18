ed08b46ef5821235a9a90ed3a09c8aa9c810f4b8
Anthony Bennett <goodoneanthony@gmail.com>
2022-12-17 23:53:09 -0800

    feat: add more keymaps

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -1,6 +1,9 @@
 vim.keymap.set('n', '<Leader>vsp', '<C-w>v')
 vim.keymap.set('n', '<Leader>sp', '<C-w>s')
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set('n', '<Leader>b', '<C-o>')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
@@ -10,3 +13,12 @@ vim.keymap.set('n', 'x', '"_x')
 
 vim.keymap.set('n', '+', '<C-a>')
 vim.keymap.set('n', '-', '<C-x>')
+
+-- to the clipboard! but not on windows
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- what is this even
+vim.keymap.set("n", "Q", "<nop>")
