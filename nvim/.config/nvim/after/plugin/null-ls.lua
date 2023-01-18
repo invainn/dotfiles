bff6efbec3adb94172b01a2f4e1587f2b7be1c1b
Anthony Bennett <goodoneanthony@gmail.com>
2023-01-17 20:54:29 -0800

    feat: remove spell completion

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -24,7 +24,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell,
+    -- null_ls.builtins.completion.spell,
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.jq,
     -- null_ls.builtins.formatting.eslint_d,
