2f7fe197b85e7c3e3703c5f9cb8942bacf9ee752
Anthony Bennett <goodoneanthony@gmail.com>
2023-01-22 01:47:50 -0800

    feat(colors): change colors to macchiato

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -2,7 +2,7 @@ local status, catppuccin = pcall(require, "catppuccin")
 if (not status) then return end
 
 catppuccin.setup({
-  flavour = "frappe",
+  flavour = "macchiato",
   term_colors = false,
   integrations = {
     native_lsp = {
